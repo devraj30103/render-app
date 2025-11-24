7e3644ebb5cd09672bac2325b38774978c518aaf
Devraj Goraksh Shirsath <119668617+devraj30103@users.noreply.github.com>
2025-11-24 23:22:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
+todos = []
+
 @app.route("/")
 def home():
-    return "<h1 style='color: #4CAF50;'>Hello Render! 🚀</h1><p>Your Flask App is Running Successfully.</p>"
+    return "<h2>Simple To-Do List</h2><br>" + "<br>".join(todos)
 
-@app.route("/about")
-def about():
-    return "<h2 style='color: #2196F3;'>About Page</h2><p>This is a colorful Flask app example!</p>"
+@app.route("/add")
+def add():
+    task = request.args.get("task")
+    if task:
+        todos.append(task)
+        return f"Added: {task}"
+    return "Please add task like /add?task=BuyMilk"
+
+@app.route("/list")
+def list_tasks():
+    return "<br>".join(todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
